cd62a5f54f872c77fa59c41c145ca5ef5b53969d
stshauke <tshaukemulumba@yahoo.com>
2024-03-20 11:50:40 +0100

    Mise à jour entité annonce

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -10,15 +10,46 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/annonce')]
 class AnnonceController extends AbstractController
 {
     #[Route('/', name: 'app_annonce_index', methods: ['GET'])]
-    public function index(AnnonceRepository $annonceRepository): Response
+    public function index(Request $request,AnnonceRepository $annonceRepository, PaginatorInterface $paginator): Response
     {
+
+          // Récupérer le terme de recherche depuis la requête
+            $searchTerm = $request->query->get('search');
+
+            // Récupérez la page actuelle à partir de la requête
+            $currentPage = $request->query->getInt('page', 1);
+
+            // Nombre d'éléments par page
+            $itemsPerPage = 6;
+        // Créez une requête Doctrine pour récupérer les annonces (alias: an)
+            $queryBuilder = $annonceRepository->createQueryBuilder('an')
+            ->orderBy('an.id', 'ASC');
+            
+            // Ajoutez une condition de recherche si un terme de recherche est spécifié
+            if ($searchTerm) {
+                $queryBuilder
+                    ->leftJoin('an.Utilisateur', 'u')
+                    ->andWhere('an.descriptionAnnonce LIKE :searchTerm OR an.lieuTravail LIKE :searchTerm  
+                    OR an.dateAnnonce LIKE :searchTerm OR u.nom LIKE :searchTerm OR u.prenom LIKE :searchTerm')
+                    ->setParameter('searchTerm', '%'.$searchTerm.'%');
+            }
+        
+        // Paginez les résultats
+        $pagination = $paginator->paginate(
+            $queryBuilder,
+            $currentPage,
+            $itemsPerPage
+        );
+        // Transférez la pagination à votre vue Twig
         return $this->render('annonce/index.html.twig', [
-            'annonces' => $annonceRepository->findAll(),
+            // 'annonces' => $annonceRepository->findAll(),
+            'pagination' => $pagination,
         ]);
     }
 
