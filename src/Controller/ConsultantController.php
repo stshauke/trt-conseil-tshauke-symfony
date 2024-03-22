9f4e6c236d5a0b7f3aaceb6754fd2664ae4ab2c5
stshauke <tshaukemulumba@yahoo.com>
2024-03-22 18:22:17 +0100

    Module consultant

---
+++ src/Controller/ConsultantController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
+
+#[Route('/consultant')]
+class ConsultantController extends AbstractController
+{
+    #[Route('/', name: 'app_consultant_index', methods: ['GET'])]
+    public function index(Request $request,UtilisateurRepository $utilisateurRepository, PaginatorInterface $paginator): Response
+    {
+        // Récupérer le terme de recherche depuis la requête
+        $searchTerm = $request->query->get('search');
+
+        // Récupérez la page actuelle à partir de la requête
+        $currentPage = $request->query->getInt('page', 1);
+
+        // Nombre d'éléments par page
+        $itemsPerPage = 6;
+    // Créez une requête Doctrine pour récupérer les annonces (alias: an)
+        $queryBuilder = $utilisateurRepository->createQueryBuilder('u')
+        ->orderBy('u.id', 'ASC');
+        
+        // Ajoutez une condition de recherche si un terme de recherche est spécifié
+        if ($searchTerm) {
+            $queryBuilder
+                ->andWhere('u.email LIKE :searchTerm OR u.nom LIKE :searchTerm OR u.prenom LIKE :searchTerm OR u.status LIKE :searchTerm OR u.roles LIKE :searchTerm OR u.dateCreation LIKE :searchTerm')
+                ->setParameter('searchTerm', '%'.$searchTerm.'%');
+        }
+        
+        
+    
+    // Paginez les résultats
+    $pagination = $paginator->paginate(
+        $queryBuilder,
+        $currentPage,
+        $itemsPerPage
+    );
+    // Transférez la pagination à votre vue Twig
+        
+        
+        
+        
+        return $this->render('consultant/index.html.twig', [
+            'pagination' => $pagination,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_consultant_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UserPasswordHasherInterface $userPasswordHasher,  EntityManagerInterface $entityManager): Response
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+             // encode the plain password
+             $utilisateur ->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $utilisateur ,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_consultant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('consultant/new.html.twig', [
+            'utilisateur' => $utilisateur,
+            'form' => $form,
+            'button_label' => 'Enregistrer',
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_consultant_show', methods: ['GET'])]
+    public function show(Utilisateur $utilisateur): Response
+    {
+        return $this->render('consultant/show.html.twig', [
+            'utilisateur' => $utilisateur,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_consultant_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Utilisateur $utilisateur, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_consultant_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('consultant/edit.html.twig', [
+            'utilisateur' => $utilisateur,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_consultant_delete', methods: ['POST'])]
+    public function delete(Request $request, Utilisateur $utilisateur, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$utilisateur->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($utilisateur);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_consultant_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -4,24 +4,64 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UtilisateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+        ->add('email', TextType::class, [
+            'label' => 'Email'
+        ])
+            ->add('agreeTerms', CheckboxType::class, [
+                                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter nos conditions.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'Mot de passe',
+                                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
             ->add('nom')
             ->add('prenom')
             ->add('dateCreation')
             ->add('status')
-        ;
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Consultant' => 'ROLE_CONSULTANT',
+                    // Ajoutez d'autres rôles si nécessaire
+                ],
+                'multiple' =>true, // Permettre à l'utilisateur de sélectionner plusieurs rôles
+                'expanded' =>true, // Afficher les choix comme des cases à cocher
+                'data' => ['ROLE_CONSULTANT'], //Définir la valeur par défaut
+            ]);
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
